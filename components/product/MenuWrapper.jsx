8ccfe56d179afb567f23ccc9413d58be0406d6be
İbrahim Okumuş <ibrahimokumus_09@hotmail.com>
2025-05-08 12:28:10 +0300

    fixed and edited some issues

--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -5,8 +5,11 @@ import MenuItem from "./MenuItem";
 const MenuWrapper = ({ categoryList, productList }) => {
 	const [activeIndex, setActiveIndex] = useState(0);
 	const [filter, setFilter] = useState([]);
+	const [productLimit, setProductLimit] = useState(3);
 	useEffect(() => {
-		setFilter(productList.filter((product) => product.category === categoryList[activeIndex].title.toLowerCase()));
+		setFilter(productList?.filter((product) => product.category === categoryList[activeIndex]?.title?.toLowerCase()));
+
+		setProductLimit(3);
 	}, [categoryList, productList, activeIndex]);
 
 	//console.log(productList);
@@ -15,24 +18,24 @@ const MenuWrapper = ({ categoryList, productList }) => {
 			<div className="flex flex-col items-center ">
 				<Title addClass="text-[40px]"> Our Menu </Title>
 				<div className="mt-10">
-					{categoryList.length > 0 &&
-						categoryList.map((category, index) => (
-							<button
-								className={`px-6 py-2 rounded-3xl ${
-									index === activeIndex && "bg-secondary text-white"
-								} `}
-								key={category._id}
-								onClick={() => setActiveIndex(index)}
-							>
+					{categoryList?.length > 0 &&
+						categoryList?.map((category, index) => (
+							<button className={`px-6 py-2 rounded-3xl ${index === activeIndex && "bg-secondary text-white"} `} key={category._id} onClick={() => setActiveIndex(index)}>
 								{category.title}
 							</button>
 						))}
 				</div>
 			</div>
 
-			<div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[450px]">
-				{filter?.length > 0 && filter.map((product) => <MenuItem key={product._id} product={product} />)}
-			</div>
+			<div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[450px]">{filter?.length > 0 && filter.slice(0, productLimit).map((product) => <MenuItem key={product._id} product={product} />)}</div>
+
+			{filter?.length > 0 && (
+				<div className="flex justify-center mt-8">
+					<button className="btn-primary" onClick={() => setProductLimit(productLimit + 3)}>
+						View more
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
